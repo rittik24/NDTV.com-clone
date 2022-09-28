c0f2bbde84765d8f213fb1f1be995e747bdf58f1
Rejaul Khan <rejaulalikhan05@gmail.com>
2022-09-29 02:11:20 +0530

    trenspage add

---
+++ trend_page/index.js
@@ -0,0 +1,271 @@
+let data = [
+  {
+    img: "https://c.ndtvimg.com/2021-06/9924tgg_ekta-kapoor-_120x90_07_June_21.jpg",
+    headline:
+      "Arrest Warrant Against Producer Ekta Kapoor, Mother Over Web Series",
+    p1: "Updated: September 28, 2022",
+    p2: "It has been alleged in the complaint that the series 'XXX' (Season-2) ..",
+    path: "https://www.ndtv.com/india-news/arrest-warrant-against-producer-ekta-kapoor-mother-over-web-series-3386334#trendingnow",
+  },
+
+  {
+    img: "https://c.ndtvimg.com/2022-09/0qeuo3qg_anil-chauhan_120x90_28_September_22.jpg",
+    headline:
+      "New Chief Of Defence Staff (CDS) Is Lt General Anil Chauhan (Retired)",
+    p1: "Updated: September 28, 2022 8:14 pm IST",
+    p2: "The position of the Chief of Defence Staff was created to integrate the three services - the Army, the Navy and the Air Force.",
+    path: "https://www.ndtv.com/offbeat/what-goes-around-comes-around-anand-mahindra-on-how-british-education-affected-colonised-countries-3382064",
+  },
+
+  {
+    img: "https://c.ndtvimg.com/2022-09/ohjgehp_harjot-kaur-bhamra-head-of-women-and-child-development-corporation-at-tuesdays-function-in-patna_120x90_28_September_22.jpg",
+    headline:
+      "Video: Want Condoms Too? Bihar Officer's Shocker On Girl's Sanitary Pad Query",
+    p1: "Updated: September 28, 2022",
+    p2: "At Patna event on 'enhancing value of girls'. ",
+    path: "https://www.ndtv.com/india-news/want-condoms-too-bihar-officer-to-schoolgirl-on-sanitary-pad-question-invokes-pakistan-3385283#trendingnow",
+  },
+
+  {
+    img: "https://c.ndtvimg.com/2022-09/oe2286ss_jackie-walorski_120x90_28_September_22.jpg",
+    headline:
+      "Watch: Where's Jackie? Joe Biden Asks For Lawmaker Who Died In Crash In AugustAmid Anti-Hijab Protests, Iranian Sisters' Persian Version Of Bella Ciao Amazes Internet",
+    p1: "Updated: September 28, 2022",
+    p2: "Biden issued a statement at the time of her death, saying that he and the first lady were -shocked and saddened- by the news.",
+    path: "https://www.ndtv.com/world-news/wheres-jackie-at-hunger-forum-joe-biden-asks-for-lawmaker-who-died-in-august-3386801#trendingnow",
+  },
+  {
+    img: "https://c.ndtvimg.com/2022-09/upmirnd_girl-meets-rahul-gandhi-bharat-jodo-240_120x90_28_September_22.jpg",
+    headline:
+      "Viral Video: Girl Can't Control Her Tears On Meeting Rahul Gandhi During 'Bharat Jodo Yatra'",
+    p1: "Updated: September 28, 2022",
+    p2: "Rahul Gandhi is leading the Congress party's 'Bharat Jodo Yatra'.",
+    path: "https://www.ndtv.com/india-news/viral-video-girl-cant-control-her-tears-on-meeting-rahul-gandhi-during-bharat-jodo-yatra-3385711#trendingnow",
+  },
+
+  {
+    img: "https://c.ndtvimg.com/2022-09/kgkpfdug_ar-rahman_625x300_28_September_22.jpg",
+    headline: "AR Rahman Reacts To Remix Culture: Who Are You To Re-Imagine",
+    p1: "Updated: September 28, 2022",
+    p2: "The more I see, the more it is distorted,he said",
+    path: "https://www.ndtv.com/entertainment/ar-rahman-reacts-to-remix-culture-who-are-you-to-re-imagine-3385825#trendingnow",
+  },
+  {
+    img: "https://c.ndtvimg.com/2022-08/re6ca9nk_anu-tyagi_625x300_10_August_22.jpg",
+    headline:
+      "Arrested Noida Politician's Wife Accuses BJP MP Of Threatening Her",
+    p1: "Updated: September 28, 2022 10:10 pm IST",
+    p2: "The allegations of Anu Tyagi against the BJP leader and Lok Sabha MP came during her address to theTyagi community members who had gathered in protest outside her Grand Omaxe society in Noida.",
+    path: "https://www.ndtv.com/noida-news/arrested-noida-politician-shrikant-tyagis-wife-accuses-bjp-mp-mahesh-sharma-of-threatening-her-3386645#trendingnow",
+  },
+  {
+    img: "https://c.ndtvimg.com/2022-09/eeb779k8_kl-rahul-suryakumar-yadav-bcci_625x300_28_September_22.jpg?im=FeatureCrop,algorithm=dnn,width=240,height=180",
+    headline:
+      "India vs South Africa, 1st T20I Highlights: Arshdeep Singh Shines With Ball, KL Rahul, Suryakumar Yadav Fifties Help India Win",
+    p1: "Updated: September 28, 2022",
+    p2: "IND vs SA, 1st T20I Highlights: Chasing a target of 107.",
+    path: "https://sports.ndtv.com/india-vs-south-africa-2022-23/ind-vs-sa-live-cricket-score-1st-t20-match-india-vs-south-africa-live-score-updates-3385482#trendingnow",
+  },
+
+  {
+    img: "https://c.ndtvimg.com/2022-09/0nakfnjo_ashok-gehlot-pti_625x300_03_September_22.jpg",
+    headline:
+      "Ashok Gehlot In Delhi To Meet Sonia Gandhi, Key Aide Says He Won't Resign",
+    p1: "Edited by  Bhavya Sukheja | Tuesday September 27, 2022",
+    p2: "Over 90 MLAs had on Sunday threatened mass resignation.",
+    path: "https://www.ndtv.com/india-news/rajasthan-congress-crisis-ashok-gehlot-likely-to-meet-sonia-gandhi-today-3384350#trendingnow",
+  },
+
+  {
+    img: "https://c.ndtvimg.com/2022-09/padfpl7_hadis-najafi_120x90_28_September_22.jpg",
+    headline:
+      "Iran Woman Shot Dead After Viral Video of Unscarved Hair: Report",
+    p1: "Edited by Diksha Rani | Monday September 26, 2022",
+    p2: "A video of her funeral has gone viral.",
+    path: "https://www.ndtv.com/offbeat/viral-video-pastry-chef-creates-pinata-internet-calls-him-bob-ross-of-chocolate-3379693",
+  },
+  {
+    img: "https://c.ndtvimg.com/2022-09/h5e6tt3_attorney-general-r-venkataramani-650_650x400_28_September_22.jpg",
+    headline:
+      "New Attorney General Of India Is Senior Advocate R Venkataramani",
+    p1: "Updated: September 28, 2022 4:09 pm IST",
+    p2: "R Venkataramani will replace current Attorney General KK Venugopal whose term ends on September 30, 2022",
+    path: "https://www.ndtv.com/india-news/new-attorney-general-of-india-is-senior-advocate-r-venkatramani-3386462#trendingnow",
+  },
+  {
+    img: "https://c.ndtvimg.com/2021-02/4fbnrc3_money-generic-rupees-generic_120x90_25_February_21.jpg",
+    headline:
+      "4% Dearness Allowance Hike, Festive Cheer For 47 Lakh Central Employees",
+    p1: "Edited by NDTV News Desk | Monday September 26, 2022",
+    p2: "In 1922, British archaeologist Howard Carter discovered the 3,300-year-old burial chamber of King Tutankhamun.",
+    path: "https://www.ndtv.com/business/dearness-allowance-da-hiked-by-4-for-central-government-employees-pensioners-sources-3384645#trendingnow",
+  },
+
+  {
+    img: "https://c.ndtvimg.com/2022-09/mepna83g_ghaziabad-police-arrests-21-people_120x90_28_September_22.jpg",
+    headline:
+      "21 Arrested For Celebrating Birthday Party On Flyover Near Delhi",
+    p1: "Edited by NDTV News Desk | Monday September 26, 2022",
+    p2: "The accused obstructed the traffic movement by parking their car in a haphazard manner.",
+    path: "https://www.ndtv.com/india-news/21-arrested-for-celebrating-birthday-party-on-flyover-near-delhi-3386434#trendingnow",
+  },
+];
+
+function readArticle(elem) {
+  localStorage.setItem("readThisArticle", JSON.stringify(elem));
+}
+
+data.forEach(function (elem) {
+  let div = document.createElement("div");
+  div.addEventListener("click", function () {
+    window.location.href = elem?.path;
+    let dataObj = {
+      category: "Offbeat",
+      description: `${elem.p1} ${elem.p2}`,
+      headline: `${elem.headline}`,
+      imageUrl: elem.img,
+      postedDate: new Date().toISOString().slice(0, 10),
+    };
+    readArticle(dataObj);
+  });
+  let div1 = document.createElement("div");
+  let img = document.createElement("img");
+  img.setAttribute("src", elem.img);
+  let div2 = document.createElement("div");
+  let h3 = document.createElement("h3");
+  h3.innerText = elem.headline;
+  let p1 = document.createElement("p");
+  p1.innerText = elem.p1;
+  let p2 = document.createElement("p");
+  p2.innerText = elem.p2;
+
+  div1.setAttribute("class", "image");
+  div2.setAttribute("class", "innertext");
+  document.querySelector("#box2").append(div);
+  div.append(div1, div2);
+  div1.append(img);
+  div2.append(h3, p1, p2);
+});
+
+// ---------------------
+
+// ----------------
+let data2 = [
+  {
+    img: "https://c.ndtvimg.com/2022-09/ohjgehp_harjot-kaur-bhamra-head-of-women-and-child-development-corporation-at-tuesdays-function-in-patna_120x90_28_September_22.jpg",
+    p: "Video: Want Condoms Too Bihar Officer-s Shocker On Girl-s Sanitary Pad Query",
+    path: "https://www.ndtv.com/india-news/want-condoms-too-bihar-officer-to-schoolgirl-on-sanitary-pad-question-invokes-pakistan-3385283#trendingnow",
+  },
+  {
+    img: "https://c.ndtvimg.com/2022-09/ohjgehp_harjot-kaur-bhamra-head-of-women-and-child-development-corporation-at-tuesdays-function-in-patna_120x90_28_September_22.jpg",
+    p: "Video: Condoms Too? Bihar Officer To Schoolgirl On Sanitary Pad Query",
+    path: "https://www.ndtv.com/india-news/want-condoms-too-bihar-officer-to-schoolgirl-on-sanitary-pad-question-invokes-pakistan-3385283#News_Trending",
+  },
+  {
+    img: "https://c.ndtvimg.com/2022-09/h852d5hg_sobhitaethnicmakeup1_120x90_27_September_22.jpg",
+    p: "Beauty Sobhita Dhulipala's Ethnic Makeup",
+    path: "https://swirlster.ndtv.com/webstories/beauty/sobhita-dhulipalas-ethnic-makeup-6987?utm_source=www.ndtv.com&utm_medium=web-page&utm_campaign=webstories",
+  },
+  {
+    img: "https://c.ndtvimg.com/2022-09/u6lh50fc_navratricelebsthumb_120x90_27_September_22.jpg",
+    p: "Entertainment  Navratri Colours, As Seen On Celebs",
+    path: "https://movies.ndtv.com/webstories/entertainment/navratri-colours-as-seen-on-celebs-6983?utm_source=www.ndtv.com&utm_medium=web-page&utm_campaign=webstories",
+  },
+
+  {
+    img: "https://c.ndtvimg.com/2022-09/g7pb86do_malayalam-actors-at-kerala-mall_240x180_28_September_22.jpg",
+    p: "Video Of Malayalam Actors Being Groped In Kerala Mall Prompts Probe",
+    path: "https://www.ndtv.com/kerala-news/video-of-malayalam-actors-being-groped-at-kerala-mall-prompts-probe-3384171#News_Trending",
+  },
+  {
+    img: "https://c.ndtvimg.com/2022-09/1ht7kki_sabudana-aloo-chilla_120x90_26_September_22.jpg",
+    p: "Food Navratri Special Sabudana Aloo Chilla",
+    path: "https://food.ndtv.com/webstories/food/navratri-special-sabudana-aloo-chilla-6968?utm_source=www.ndtv.com&utm_medium=web-page&utm_campaign=webstories",
+  },
+];
+
+data2.forEach(function (elem) {
+  let div = document.createElement("div");
+  div.addEventListener("click", function () {
+    window.location.href = elem?.path;
+    let dataObj = {
+      category: "Offbeat",
+      description: `${elem.p}`,
+      headline: `${elem.p}`,
+      imageUrl: elem.img,
+      postedDate: new Date().toISOString().slice(0, 10),
+    };
+    readArticle(dataObj);
+  });
+  let img = document.createElement("img");
+  img.setAttribute("src", elem.img);
+
+  let p = document.createElement("p");
+  p.innerText = elem.p;
+
+  div.append(img, p);
+  document.querySelector("#box1").append(div);
+  div.setAttribute("class", "inner");
+});
+
+let data3 = [
+  {
+    img: "https://c.ndtvimg.com/2022-09/ukhddivo_sonia_625x300_22_September_22.jpg",
+    text: "प्राण के साथ दिख रही यह हसीना ‘किसिंग गर्ल’ के नाम से थी मशहूर, फिल्में छोड़ कर बन गई थी इस राज घराने की राजमाता",
+    path: "https://ndtv.in/bollywood/this-beautiful-lady-seen-with-pran-was-a-famous-actress-had-left-films-and-married-a-prince-3367087?utm_source=TBKB&utm_medium=TBexchange&utm_campaign=Taboola&tblci=GiAxtYfxTTW7SsaAgjFCDMjpsn_L9FPfCM4mCGSbKMi4QSDQ1z4omruI9Y-zscF-",
+  },
+  {
+    img: "https://c.ndtvimg.com/2022-09/tntlt7gg_rupali-had-married-iqbal-sheikh-three-years-ago_625x300_27_September_22.jpg",
+    text: "​HomeMumbaiMumbai Man Allegedly Stabs Estranged Wife To Death Over Not Wearing Burqa ",
+    path: "https://www.ndtv.com/mumbai-news/mumbai-taxi-driver-kills-estranged-wife-over-not-following-muslim-customs-3381569",
+  },
+  {
+    img: "https://c.ndtvimg.com/2022-09/7cmv2rho_celina-jaitley-shriya-saran_625x300_21_September_22.jpg",
+    text: "देसी हीरो को छोड़ विदेशियों पर आया बॉलीवुड की इन पांच एक्ट्रेस का दिल, शादी कर बनाया जीवनसाथी",
+    path: "https://ndtv.in/bollywood/priyanka-chopra-to-radhika-apte-these-five-bollywood-actress-got-married-foreigners-3365589?utm_source=TBKB&utm_medium=TBexchange&utm_campaign=Taboola&tblci=GiAxtYfxTTW7SsaAgjFCDMjpsn_L9FPfCM4mCGSbKMi4QSDQ1z4oya-SuIrc8qx6",
+  },
+  {
+    img: "https://c.ndtvimg.com/2022-08/dktpqdjc_yogi-adityanathtwitter_625x300_03_August_22.jpg",
+    text: "Ban On Popular Front Of India (PFI) Commendable, Says Yogi Adityanath",
+    path: "https://www.ndtv.com/india-news/ban-on-popular-front-of-india-pfi-commendable-says-yogi-adityanath-3385378",
+  },
+  {
+    img: "https://c.ndtvimg.com/2020-10/j526stv_kapil-dev-afp_625x300_23_October_20.jpg?im=FeatureCrop,algorithm=dnn,width=806,height=605",
+    text: "Instead Of Intense Debates...: Kapil Dev Suggests A Better Solution After Deepti Sharma's Run-Out Of Charlie Dean",
+    path: "https://sports.ndtv.com/cricket/instead-of-intense-debates-kapil-dev-suggests-a-better-solution-after-deepti-sharmas-run-out-of-charlie-dean-3378874",
+  },
+
+  {
+    img: "https://c.ndtvimg.com/2022-09/bjfc7dq8_gauri-_625x300_26_September_22.jpg",
+    text: "शाहरुख की शर्टलेस फोटो पर वाइफ गौरी का आया रिएक्शन, बोलीं- ओह गॉड. अब वह अपने शर्ट से भी...",
+    path: "https://ndtv.in/bollywood/shah-rukh-khan-wrote-romantic-note-for-his-shirt-wife-gauri-khan-wrote-epic-reaction-3377570?utm_source=TBKB&utm_medium=TBexchange&utm_campaign=Taboola&tblci=GiAxtYfxTTW7SsaAgjFCDMjpsn_L9FPfCM4mCGSbKMi4QSDQ1z4ol7rMn5reu9xg",
+  },
+
+  {
+    img: "https://c.ndtvimg.com/2022-09/9ahfrrbo_sonakshi_625x300_22_September_22.jpg",
+    text: "Double XL Teaser: Sonakshi Sinha And Huma Qureshi Promise 'A Story Of Friendship'",
+    path: "https://www.ndtv.com/entertainment/double-xl-teaser-sonakshi-sinha-and-huma-qureshi-promise-a-story-of-friendship-3368559",
+  },
+
+  {
+    img: "https://c.ndtvimg.com/2022-09/oknbr8mg_-anusha-dandekar_625x300_20_September_22.jpg",
+    text: "When Anusha Dandekar Officiated Her Friends' Wedding In Sydney",
+    path: "https://www.ndtv.com/entertainment/when-anusha-dandekar-officiated-her-friends-wedding-in-sydney-3360993",
+  },
+];
+
+data3.forEach(function (el) {
+  let div = document.createElement("div");
+  div.addEventListener("click", function () {
+    window.location.href = el?.path;
+  });
+  let img = document.createElement("img");
+  img.setAttribute("src", el.img);
+  let h4 = document.createElement("h4");
+  h4.innerText = el.text;
+
+  div.append(img, h4);
+  document.querySelector("#box3").append(div);
+
+  div.setAttribute("class", "innerBox");
+});
