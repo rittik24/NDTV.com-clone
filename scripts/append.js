8636e71a2041754bbe8ee4f56dc8a97140f3dedb
Rittik Haldar <rickhaldar30@gmail.com>
2022-09-30 16:06:23 +0530

    opinionPage done

---
+++ scripts/append.js
@@ -0,0 +1,89 @@
+const appendMainNews = (data,container) => {
+    
+    container.innerHTML = null;
+ 
+     data.forEach(function(el){
+ 
+       let div = document.createElement("div");
+       div.id="div_dream";
+       
+       let div3 = document.createElement("div");
+       div3.id="div3_dream"
+ 
+       let image = document.createElement("img");
+       image.src=el.urlToImage;
+       image.id="mainImage_dream"
+       
+       let div2 = document.createElement("div");
+       div2.id="div2_dream"
+ 
+       let title = document.createElement("h3");
+       title.innerText = el.title;
+       
+       let description = document.createElement("p");
+       description.innerText = el.description;
+       
+       let posted_date = document.createElement("p");
+       posted_date.innerText = el.source.name+" | "+el.publishedAt;
+     
+       div2.append(title,posted_date,description);
+       div3.append(image);
+       div.append(div3,div2);
+       container.append(div);
+     })
+ };
+ 
+ export {appendMainNews};
+ 
+ 
+ const appendTrendingNews = (data,container) => {
+     
+      container.innerHTML = null;
+ 
+      let trendbtn_dream = document.createElement("button");
+      trendbtn_dream.id="trendDiv_dream"
+      trendbtn_dream.innerText = "TRENDING";
+      container.append(trendbtn_dream);
+ 
+ 
+      data.forEach(function(el){
+  
+        let chbox = document.createElement("div");
+        chbox.id="chbox_dream";
+  
+        let img = document.createElement("img");
+        img.src=el.urlToImage;
+  
+        let heading = document.createElement("h5");
+        heading.innerText = el.title;
+       
+       
+        chbox.append(img,heading);
+        container.append(chbox);
+      })
+  };
+ 
+  export {appendTrendingNews};
+ 
+  const appendAddNews = (data,container) => {
+     
+   container.innerHTML = null;
+ 
+   data.forEach(function(el){
+ 
+     let smbox = document.createElement("div");
+     smbox.id="smbox_dream";
+ 
+     let img = document.createElement("img");
+     img.src=el.urlToImage;
+ 
+     let text = document.createElement("h4");
+     text.innerText = el.title;
+    
+    
+     smbox.append(img,text);
+     container.append(smbox);
+   })
+ };
+ 
+ export {appendAddNews};
---
+++ scripts/opinion.js
@@ -0,0 +1,50 @@
+import {appendAddNews, appendMainNews, appendTrendingNews } from "../scripts/append.js";
+
+const latestData = async() => {
+  
+    try{
+     let main_container_dream = document.getElementById("mainNews_dream");
+
+     let res = await fetch('https://newsapi.org/v2/everything?q=opinion&apiKey=de829986749e4d2b88a27db3a10d200e');
+     let data = await res.json();
+     let actualData = data.articles;
+     console.log("maindata:",actualData);
+     appendMainNews(actualData,main_container_dream)
+    }catch(err){
+        console.log(err);
+    }
+}
+latestData();
+
+const trendingData = async() => {
+  
+    try{
+     let left_side_container = document.getElementById("left-side-div_dream");
+     
+     let res = await fetch('https://newsapi.org/v2/everything?q=trending&apiKey=de829986749e4d2b88a27db3a10d200e');
+     let data = await res.json();
+     let actualData = data.articles
+     console.log("trending:",actualData);
+     appendTrendingNews(actualData,left_side_container)
+    }catch(err){
+        console.log(err);
+    }
+};
+  trendingData();
+  
+ const addNews = async() => {
+    
+    try{
+        let right_side_container = document.getElementById("right-side-div_dream");
+        
+        let res = await fetch('https://newsapi.org/v2/everything?q=add&apiKey=de829986749e4d2b88a27db3a10d200e');
+        let data = await res.json();
+        let actualData = data.articles
+        console.log("add-data:",actualData);
+        appendAddNews(actualData,right_side_container);
+       }catch(err){
+           console.log(err);
+       }
+      
+        };
+       addNews();
